37d1c509191093b1c046a5fabbd1952a5432bd61
MarcialFantini <marcialfantini@gmail.com>
2022-06-14 16:36:35 -0300

    primer commit

--- src/storeRedux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import themesGlobals from "./slice/themesGlobals/themesGlobalsSlice";
-
-
-export const store = configureStore({
-    reducer: {
-        themes: themesGlobals
-    }
-})
