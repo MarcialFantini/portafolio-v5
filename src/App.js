bbcd5872e3b91728d634d4df8afb9d9c845d59b2
MarcialFantini <marcialfantini@gmail.com>
2022-06-15 03:18:10 -0300

    Agregue un nuevo trabajo hecho =)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Portada from './components/Portada';
 import WorkTodoo from './components/WorkTodoo';
+import XState from './components/XState';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Portada />
       <WorkTodoo />
+      <XState />
     </div>
   );
 }
